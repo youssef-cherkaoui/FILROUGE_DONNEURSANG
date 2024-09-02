d443c137923a851e7013fbf6736adbbff5688d12
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-02 15:49:41 +0100

    Creation du projet

---
+++ FIL_ROUGE/src/test/java/fil_rouge/FilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package fil_rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
