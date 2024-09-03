023bb40871c8f0feaf2d57c271dcb752176efb86
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-03 11:06:01 +0100

    Creation du personneModel

---
+++ FIL_ROUGE/src/main/java/fil_rouge/Model/Personne.java
@@ -0,0 +1,70 @@
+package fil_rouge.Model;
+
+import fil_rouge.Enums.GroupSanguin;
+import fil_rouge.Enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "person", discriminatorType = DiscriminatorType.STRING)
+public class Personne implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom ;
+    private String email;
+    private String motdepasse;
+    private String telephone ;
+    @Enumerated(EnumType.STRING)
+    private GroupSanguin groupSanguin;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return motdepasse;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
