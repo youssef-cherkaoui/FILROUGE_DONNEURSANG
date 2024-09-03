bef099510a0cecfc5a81f7911a7182657d742602
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-03 11:15:17 +0100

    Creation du Controller

---
+++ FIL_ROUGE/src/main/java/fil_rouge/Controller/PersonneController.java
@@ -0,0 +1,13 @@
+package fil_rouge.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class PersonneController {
+}
