be9158ebb850a7a9c99f4a5dfe12e68cb232ad48
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-03 11:08:33 +0100

    Creation du CentreCollectModel

---
+++ FIL_ROUGE/src/main/java/fil_rouge/Model/CentreCollectModel.java
@@ -0,0 +1,35 @@
+package fil_rouge.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CentreCollectModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomCentre;
+
+    @OneToMany(mappedBy = "centreCollecte")
+    @JsonIgnore
+    private List<SecretaireModel> secretaires;
+
+    @OneToMany(mappedBy = "centreCollecte")
+    @JsonIgnore
+    private List<RendezVousModel> rendezVous;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "adresse_id")
+    private AdresseModel adresse;
+}
