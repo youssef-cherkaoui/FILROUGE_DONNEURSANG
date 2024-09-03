ed0a3e4cf18620ed1c4a3d4f9dc6612c842b85db
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-03 11:15:00 +0100

    Creation du Controller

---
+++ FIL_ROUGE/src/main/java/fil_rouge/Controller/CentreCollectController.java
@@ -0,0 +1,52 @@
+package fil_rouge.Controller;
+
+import fil_rouge.Model.AdresseModel;
+import fil_rouge.Model.CentreCollectModel;
+import fil_rouge.Service.CentreCollectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "*")
+public class CentreCollectController {
+
+    @Autowired
+    private CentreCollectService centreCollectService;
+
+    @PostMapping("/AddCentre")
+    public CentreCollectModel AddCentreCollect(@RequestBody CentreCollectModel centreCollectModel) {
+        return centreCollectService.AddCentreCollect(centreCollectModel);
+    }
+
+    @GetMapping("/ShowAllCentre")
+    public List<CentreCollectModel> getAllCentreCollect() {
+        return centreCollectService.getAllCentreCollect();
+    }
+
+    @GetMapping("/SearchCentre")
+    public List<CentreCollectModel> findByNomCentre(@RequestParam String nomCentre) {
+        return centreCollectService.findByNomCentre(nomCentre);
+    }
+
+    @PutMapping("/UpdateCentre/{id}")
+    public CentreCollectModel updateCentre(@PathVariable Long id, @RequestBody CentreCollectModel centreCollectModel) {
+        return centreCollectService.updateCentre(id,centreCollectModel);
+    }
+
+    //Endpoint pour assigner une adresse à un centre
+    @PostMapping("/{CentreId}/adresse")
+    public CentreCollectModel AssignAdresseToCentre(@PathVariable Long id, @RequestBody AdresseModel adress){
+        return centreCollectService.AssignAdresseToCentre(id,adress);
+    }
+
+
+    @GetMapping("/{CentreId}/adresse")
+    public AdresseModel getAdressByCentre(@PathVariable Long id){
+        return centreCollectService.getAdressByCentre(id);
+    }
+
+
+}
