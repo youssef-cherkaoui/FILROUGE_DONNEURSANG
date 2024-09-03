5d2cc0a6035bad72b2ef5fa793628434eb4d7c16
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-03 11:15:42 +0100

    Creation du Controller

---
+++ FIL_ROUGE/src/main/java/fil_rouge/Controller/SecretaireController.java
@@ -0,0 +1,33 @@
+package fil_rouge.Controller;
+
+import fil_rouge.Model.SecretaireModel;
+import fil_rouge.Service.SecretaireService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "*")
+public class SecretaireController {
+
+    @Autowired
+    private SecretaireService secretaireService;
+
+    @PostMapping("/AjoutSecretaire")
+    public SecretaireModel AjouterSecretaire(@RequestBody SecretaireModel secretaire) {
+        return secretaireService.AjouterSecretaire(secretaire);
+    }
+
+    @GetMapping("/ShowAllSecre")
+    public List<SecretaireModel> ShowAllSecretaire() {
+        return secretaireService.getAllSecretaire();
+    }
+
+    @DeleteMapping("/delete/{idSe}")
+    public void DeleteSecretaire(@PathVariable Long idSe) {
+        secretaireService.deleteSecretaire(idSe);
+    }
+
+}
