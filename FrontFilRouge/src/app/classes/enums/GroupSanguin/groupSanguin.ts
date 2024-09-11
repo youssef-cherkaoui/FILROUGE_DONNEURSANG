7cfd2826e07366e48af55cf3282541818198b54c
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-11 16:26:52 +0100

    enums

---
+++ FrontFilRouge/src/app/classes/enums/GroupSanguin/groupSanguin.ts
@@ -0,0 +1,10 @@
+export enum GroupSanguin {
+  A_POS = 'A_POS',
+  A_NEG = 'A_NEG',
+  B_POS = 'B_POS',
+  B_NEG = 'B_NEG',
+  AB_POS = 'AB_POS',
+  AB_NEG = 'AB_NEG',
+  O_POS = 'O_POS',
+  O_NEG = 'O_NEG'
+}
