d23fe87dd0b1fede36a9b7e17244d5d8d42d0901
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-09-23 17:37:53 +0100

    update AuthController

--- FIL_ROUGE/src/main/java/fil_rouge/Controller/AuthenticationController.java
+++ FIL_ROUGE/src/main/java/fil_rouge/Controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
